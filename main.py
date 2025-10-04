c6d93e6fc8f823d1b3b871f0b746ab071b4909f5
nil <dneatm@gmail.com>
2025-10-03 22:28:36 -0600

    fixed duplicate word count

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def main():
     else:
         filepath = sys.argv[1]
         num_words = get_num_words(get_book_text(filepath))
-        print(f"Found {num_words} total words")
         num_chars = get_num_chars(get_book_text(filepath))
         get_num_chars(get_book_text(filepath))
         chars = sort_chars(num_chars)
